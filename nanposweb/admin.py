21fd8ab6202b80535b83ec2d5187908629a557ba
Dominik Rimpf <dev@drimpf.de>
2021-10-05 05:12:44 +0200

    fix admin user display

--- nanposweb/admin.py
+++ nanposweb/admin.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-from .models import User, Product
+from .db import db
+from .models import User, Product, Revenue
 from .util import admin_required
 
 admin = Blueprint('admin', __name__)
@@ -11,6 +12,12 @@ admin = Blueprint('admin', __name__)
 @login_required
 @admin_required
 def index():
-    users = User.query.order_by(User.name).all()
+    aggregation = db.select(db.func.sum(Revenue.amount).label('balance'), Revenue.user.label('user_id')).group_by(
+        Revenue.user).subquery()
+    user_query = db.select(User, db.func.coalesce(aggregation.c.user_id, 0)).outerjoin(
+        aggregation,
+        User.id == aggregation.c.user_id
+    ).order_by(User.name)
+    users = db.session.execute(user_query).all()
     products = Product.query.order_by(Product.name).all()
     return render_template('admin.html', users=users, products=products)
