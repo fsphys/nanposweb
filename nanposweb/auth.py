69fc9a9bc8b4c244d64dd880aa669c7e030f010e
Dominik Rimpf <dev@drimpf.de>
2021-10-25 23:50:32 +0200

    Simplified login flow for terminal / web mode. Redirect from login if logged in

--- nanposweb/auth.py
+++ nanposweb/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app, session
-from flask_login import login_required, logout_user, login_user
+from flask_login import login_required, logout_user, login_user, current_user
 from flask_principal import identity_changed, Identity, AnonymousIdentity
 
 from .forms import LoginForm
@@ -11,6 +11,11 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('Already logged in.')
+        return redirect(request.args.get('next') or url_for('main.index'))
+
+    session['terminal'] = request.args.get('terminal', False, type=bool)
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -21,34 +26,12 @@ def login():
             flash('Logged in', 'success')
 
             identity_changed.send(current_app._get_current_object(), identity=Identity(user.id))
-            session['terminal'] = False
 
             return redirect(request.args.get('next') or url_for('main.index'))
         else:
             flash('Please check your login details and try again.', 'danger')
 
-    return render_template('login.html', form=form, terminal=False)
-
-
-@auth_bp.route('/terminal', methods=['GET', 'POST'])
-def terminal():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        user = User.query.filter_by(name=form.username.data).one_or_none()
-
-        if user and check_hash(user.pin, form.pin.data):
-            login_user(user, remember=form.remember.data)
-            flash('Logged in', 'success')
-
-            identity_changed.send(current_app._get_current_object(), identity=Identity(user.id))
-            session['terminal'] = True
-
-            return redirect(request.args.get('next') or url_for('main.index'))
-        else:
-            flash('Please check your login details and try again.', 'danger')
-
-    return render_template('login.html', form=form, terminal=True)
+    return render_template('login.html', form=form)
 
 
 @auth_bp.route('/logout')
@@ -60,13 +43,13 @@ def logout():
     for key in ('identity.id', 'identity.auth_type'):
         session.pop(key, None)
 
-    session.pop('impersonate', None)
+    session.pop('impersonate', None)  # remove impersonation status to avoid weird behaviours
 
     # Tell Flask-Principal the user is anonymous
     identity_changed.send(current_app._get_current_object(), identity=AnonymousIdentity())
     flash('Logged out', 'success')
+
     if session['terminal']:
-        session.pop('terminal', None)
-        return redirect(url_for('auth.terminal'))
-    session.pop('terminal', None)
-    return redirect(url_for('auth.login'))
+        return redirect(url_for('auth.login', terminal=True))
+    else:
+        return redirect(url_for('auth.login'))
