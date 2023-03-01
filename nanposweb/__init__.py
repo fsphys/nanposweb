6c2950c77d119a5c52c33a7f79385eabb58dd4db
Alexander Kaschta <alexander.kaschta@t-online.de>
2023-03-01 14:03:24 +0100

    FIX: Updated flask method

--- nanposweb/__init__.py
+++ nanposweb/__init__.py
@@ -91,10 +91,10 @@ def create_app(test_config=None):
 
     @nanposweb_app.context_processor
     def get_version():
-        if nanposweb_app.env == 'production':
-            version = metadata.version('nanposweb')
-        else:
+        if nanposweb_app.debug:
             version = 'devel'
+        else:
+            version = metadata.version('nanposweb')
         return dict(version=version)
 
     @nanposweb_app.context_processor
